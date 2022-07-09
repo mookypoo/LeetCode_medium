7c919f568c23c8742256d616f0949ab3b1c64cb9
Hey Soo Kim (김혜수) <85905612+sookim482@users.noreply.github.com>
2022-07-09 17:17:09 +0900

    Create container_with_most_water.js

---
+++ container_with_most_water.js
@@ -0,0 +1,14 @@
+const maxArea = function(height){
+    var list = [];
+    for (var a of height){
+        var b = a + 1;
+        for (var b of height) {
+            var area = (b - a) * (height[b] > height[a] ? height[a] : height[b]);
+            list.push(area);
+        }
+    }
+    list.sort((a, b) => b-a);
+    console.log(list);
+    return list[0];
+}
+
