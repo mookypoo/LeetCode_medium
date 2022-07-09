9cdb05e631a60f09a960a52d1b5abef41b8b08c3
Hey Soo Kim (김혜수) <85905612+sookim482@users.noreply.github.com>
2022-07-09 18:23:23 +0900

    Create 3_longest_substring.js

---
+++ 3_longest_substring.js
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+const lengthOfLongestSubstring = function(s) {
+    var list = [];
+    for (var i = 0; i < s.length; i++){
+        for (var a = i+1; a < s.length; a++){
+            var sub = s.substring(i, a);
+            var set = new Set(sub);
+            if (set.size === sub.length) list.push(set.size);
+        }
+    }
+    return list.sort((a, b) => b-a)[0];
+};
+
+lengthOfLongestSubstring("pwwkew"); // 3
+lengthOfLongestSubstring("bbb"); // 1
+lengthOfLongestSubstring("abcabcbb"); // 3
